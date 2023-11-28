575f53832ee434e58d80e75d73b42c154dcf248a
unknown <turkyilmaz.bediz@gmail.com>
2023-11-28 20:14:38 +0100

    new info

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
+        //how to use sort //
 
-// how to use forEach //
+let arr =[4,3,5,8]
+arr.sort ()
+console.log(arr)
+
+
+        //how to use For //
+
+const array = ['one', 'two', 'three'];
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+
+        // how to use forEach //
+
 const myArray = ["one", "two", "three"];
 myArray.forEach (item => {
     console.log(item);
 });
+
+
+        // how to use forEach //
+
+
+
+
+        //how to write arrow function version//
+// Function 1
+function notify(name) {
+    console.log(`${name} is here`);
+  }
+  
+  // Function 2
+  setTimeout(function() {
+    console.log("1000ms == 1 second");
+  }, 1000);
+
+        // Function 1
+const notify = (name) => {
+    console.log(`${name} is here`);
+  }
+  
+  // Function 2
+  setTimeout(() => {console.log("1000ms == 1 second");}, 1000);
